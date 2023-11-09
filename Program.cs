f4d9b4dece07dc5c5f37c16fdd7dbb1996919a81
alvarfs <alvaro.florez@estudiant.fjaverianas.com>
2023-11-09 08:37:31 +0100

    FINAL RELEASE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Escribe un numero: ");
+        int numUser = Convert.ToInt32(System.Console.ReadLine());
+
+        if (numUser > 50){
+            Console.WriteLine("Tu numero es mayor a 50!");
+        }
+
+        Console.WriteLine("Fin del programa");
+    }
+}
